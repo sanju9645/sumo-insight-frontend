460c32e4a7c02b3d99bb13b0a1b7eb91afe3ac10
Sanju S Nath <sanju@nethram.com>
2025-05-13 21:30:34 +0530

    Header scrolling issue fixed

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -2,7 +2,7 @@ import { siteContents } from "../lib/siteContents";
 
 const Footer = () => {
   return (
-    <div className={`${siteContents.colors.bgDark1} py-10 hidden md:block`}>
+    <div className={`${siteContents.colors.bgDark1} py-10 hidden md:block px-5`}>
       <div className="container mx-auto flex flex-col md:flex-row justify-between items-center">
         <span className={`text-3xl ${siteContents.colors.textLight1} font-bold tracking-tight`}>
           {siteContents.title}
