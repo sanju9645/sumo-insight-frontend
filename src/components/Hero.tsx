32f198e17533840f57964a57cdf11279f6be901e
Sanju S Nath <sanju@nethram.com>
2025-05-13 22:06:11 +0530

    lazy loading added for background image

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -4,7 +4,12 @@ import hero from "../assets/hero.webp";
 const Hero = () => {
   return (
     <div>
-      <img src={hero} className="w-full max-h-[600px] object-cover" />
+      <img 
+        src={hero} 
+        className="w-full max-h-[600px] object-cover" 
+        loading="lazy"
+        alt="Hero"
+      />
     </div>
   );
 };
